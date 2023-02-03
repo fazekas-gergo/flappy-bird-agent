f3aa05b9a80d50ab57cf54da40786421113d7fe7
Fazekas Gergő <gergo.fazekas.developer@gmail.com>
2023-02-03 21:39:58 +0100

    Cleanup unused code

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from game import Game, window
 def main(agent: Agent):
     game = Game()
     game.reset()
-    i = 0
     while pygame.QUIT not in [evt.type for evt in window.events]:
-        i += 1
         state, passed, game_over = game.update()
         if game_over:
             agent.on_game_over()
